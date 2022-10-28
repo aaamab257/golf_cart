babe4af5ffdf728f7b0cb072f1232d25b5631a9c
Ahmed Ashraf <ahmedashraf@Ahmeds-iMac-Pro.local>
2022-10-28 11:14:47 +0200

    update

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,10 @@ import os
 
 
 
+
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,6 +121,8 @@ USE_I18N = True
 USE_TZ = True
 
 
+MEDIA_URL = ''
+MEDIA_ROOT = ''
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -124,9 +130,13 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+
